76dfe0fa03c48876f72b13fb6f694b44dcb41e98
guo.changxin <guo.changxin@sfit.shfe.com.cn>
2019-12-31 14:23:31 +0800

    # feat: 添加部分路由，引入一些koa插件

--- index.js
+++ index.js
@@ -1,8 +1,69 @@
 const Koa = require('koa')
+const koaStatic = require('koa-static')
+const bodyParser = require('koa-bodyparser')
+const koaLogger = require('koa-logger')
+const cors = require('koa2-cors')
+const views = require('koa-views')
+
+
+const path = require('path')
+
 const app = new Koa()
+
+
+const index = require('./server/routes/index')
+const user = require('./server/routes/users')
+
+// 配置控制台日志中间件
+app.use(koaLogger())
+// 配置ctx.body解析中间件
+app.use(bodyParser({
+    enableTypes:['json', 'form', 'text']
+}))
+//使用跨域
+app.use(cors())
+// 配置静态资源加载中间件
+app.use(koaStatic(
+    path.join(__dirname , './../static')
+))
+// 配置服务端模板渲染引擎中间件
+app.use(views(path.join(__dirname, './views'), {
+    extension: 'ejs'
+}))
+
+// logger
+app.use(async (ctx, next) => {
+    const start = new Date()
+    await next()
+    const ms = new Date() - start
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
 
 app.use(async (ctx, next) => {
     ctx.body = 'hello koa'
 })
 
-app.listen(3300)
+
+
+app.use(index.routes(), index.allowedMethods())
+app.use(user.routes(), user.allowedMethods())
+
+
+
+
+
+
+
+
+
+// error-handling
+app.on('error', (err, ctx) => {
+    console.error('server error', err, ctx)
+});
+
+// 监听启动端口
+app.listen(3300, () => {
+    console.log('[middleware] koa2-middleware is starting at port 3300');
+})
+
+module.exports = app
---
+++ server/routes/index.js
@@ -0,0 +1,31 @@
+const Router = require('koa-router')
+const { query } = require('../utils/query')
+const {
+    CREATE_TABLE,
+    INSERT_TABLE,
+    UPDATE_TABLE,
+    DELETE_TABLE
+} = require('../utils/sql')
+
+// 初始化数据库，创建表
+// query(CREATE_TABLE)
+// 装载所有子路由
+let router = new Router();
+
+router.get('/', async (ctx, next) => {
+    await ctx.render('index', {
+        title: 'Hello Koa 2!'
+    })
+})
+
+router.get('/string', async (ctx, next) => {
+    ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+    ctx.body = {
+        title: 'koa2 json'
+    }
+})
+
+module.exports = router
