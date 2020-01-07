f96d19a49b48c8a2882a782096f6cb6fb19af254
guo.changxin <guo.changxin@sfit.shfe.com.cn>
2020-01-07 17:20:41 +0800

    # feat: mac本地安装mysql，并建库

---
+++ db.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+const mySequelize = new Sequelize('gcx','root','12345678',{
+    host:'localhost',
+    dialect:'mysql',  // 指定数据库的类型，需要先安装mysql2
+    logging: true,   //默认值为true,是否在控制台中显示具体的mysql操作
+    operatorsAliases:false,
+    dialectOptions:{
+        //字符集
+        charset:'utf8mb4',
+        collate:'utf8_general_ci',
+        supportBigNumbers: true,
+        bigNumberStrings: true
+    },
+    pool:{
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    timezone: '+08:00'  //东八时区  一定要这么设置才能按照北京时间来记录时间相关的数据
+});
+
+// force: true 当我们添加字段的时候数据库会自动删除原先的表格并更新字段,开发阶段可以设置为true
+// 注意在生产环境下千万不要将其设置为true，它会把所有的记录都删除掉
+// 所以我们还是设置为false比较安全，添加字段，我们还是手动删除原先的表格然后刷新加载比较好
+mySequelize.sync({
+    force: true
+})
+
+module.exports = {
+    mySequelize
+};
--- index.js
+++ index.js
@@ -1,38 +1,53 @@
-const Koa = require('koa')
-const koaStatic = require('koa-static')
-const bodyParser = require('koa-bodyparser')
-const koaLogger = require('koa-logger')
-const cors = require('koa2-cors')
-const views = require('koa-views')
+const Koa = require('koa');
+const koaStatic = require('koa-static');
+const bodyParser = require('koa-bodyparser');
+const koaLogger = require('koa-logger');
+const cors = require('koa2-cors');
+const views = require('koa-views');
 
 
-const path = require('path')
+const path = require('path');
 
 const app = new Koa()
 
 
-const indexRouter = require('./server/routes/index')
-const userRouter = require('./server/routes/users')
+const indexRouter = require('./server/routes/index');
+const userRouter = require('./server/routes/users');
 
-const indexAPI = new indexRouter().getRouter()
+const indexAPI = new indexRouter().getRouter();
 
+
+
+// 配置跨域
+app.use(async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With')
+    ctx.set('Access-Control-Allow-Origin', 'http://localhost:9000');
+    ctx.set('Access-Control-Allow-Methods', 'PUT,DELETE,POST,GET');
+    ctx.set('Access-Control-Allow-Credentials', true);
+    ctx.set('Access-Control-Max-Age', 3600 * 24);
+    await next();
+});
 
 // 配置控制台日志中间件
-app.use(koaLogger())
-// 配置ctx.body解析中间件
+app.use(koaLogger());
+
+// 使用表单解析中间件
 app.use(bodyParser({
     enableTypes:['json', 'form', 'text']
-}))
+}));
+
 //使用跨域
-app.use(cors())
+app.use(cors());
+
 // 配置静态资源加载中间件
 app.use(koaStatic(
     path.join(__dirname , './../static')
-))
+));
+
 // 配置服务端模板渲染引擎中间件
 app.use(views(path.join(__dirname, './views'), {
     extension: 'ejs'
-}))
+}));
 
 // logger
 app.use(async (ctx, next) => {
@@ -40,7 +55,7 @@ app.use(async (ctx, next) => {
     await next()
     const ms = new Date() - start
     console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+});
 
 // x-response-time
 app.use(async (ctx, next) => {
@@ -48,7 +63,7 @@ app.use(async (ctx, next) => {
     await next()
     const ms = Date.now() - start
     ctx.set('X-Response-Time', `${ms}ms`)
-})
+});
 
 // response
 // app.use(async (ctx, next) => {
@@ -56,16 +71,11 @@ app.use(async (ctx, next) => {
 // })
 
 
-app.use(indexAPI.routes())
+app.use(indexAPI.routes(), indexAPI.allowedMethods());
+// app.use(indexAPI.allowedMethods())
 
 // app.use(indexRouter.routes(), indexRouter.allowedMethods())
 // app.use(userRouter.routes(), userRouter.allowedMethods())
-
-
-
-
-
-
 
 
 
@@ -77,6 +87,6 @@ app.on('error', (err, ctx) => {
 // 监听启动端口
 app.listen(3300, () => {
     console.log('[middleware] koa2-middleware is starting at port 3300');
-})
+});
 
 module.exports = app
