96fec1f5d91d67817a10e79d5f597d543bf4fbfc
guo.changxin <guo.changxin@sfit.shfe.com.cn>
2020-01-09 17:01:39 +0800

    # feat: sequelize建表方式确定

--- server/models/DBConnector.js
+++ server/models/DBConnector.js
@@ -1,39 +1,20 @@
 /*
 * 数据库表关系建立
 */
-const Mysql = require('./db');
+// 引入mysql的配置文件
+const db = require('./db');
 
-//表
-const User = require('./user');//用户表
-// const AdminUser = require('./adminUser');//管理员表
-// const UserInfo = require('./userInfo');//用户信息表
-// const Article = require('./article');//文章表
-// const Category = require('./category');//文章类别表
-// const Attachment = require('./attachment');//文章附件表
+// 引入sequelize对象
+const Sequelize = db.sequelize;
+
 
 /**
  * 关系建立
  */
 
-//用户-用户资料
-// User.hasOne(UserInfo); //1:1
+const article = Sequelize.import('./article');
+article.sync({force: true});
 
-//用户-文章
-// User.hasMany(Article); //1:N
-// Article.belongsTo(User); //1:1
+const user = Sequelize.import('./user');
+user.sync({force: true});
 
-//文章-分类 （定义中间表ArticleCategory 实现多对多）
-// Article.belongsToMany(Category,{through: 'ArticleCategory'}); //N:N
-// Category.belongsToMany(Article,{through: 'ArticleCategory'}); //N:N
-
-
-//基于sequelize自动创建表//【！！注意 首次执行完请注释掉该段代码 ！！】
-Mysql.sync({
-    force: true,//是否清空数据库表
-}).then(function() {
-    console.log('ok');
-});
-
-module.exports = {
-    User: User,
-};
--- server/models/db.js
+++ server/models/db.js
@@ -1,9 +1,16 @@
 const Sequelize = require('sequelize');
-const mySequelize = new Sequelize('gcx','root','12345678',{
+const sequelize = new Sequelize('gcx','root','12345678',{
     host:'localhost',
+    port: 3306,       // 自定义端口，默认3306
     dialect:'mysql',  // 指定数据库的类型，需要先安装mysql2
-    logging: true,   //默认值为true,是否在控制台中显示具体的mysql操作
-    operatorsAliases:false,
+    logging: false,   //默认值为true,是否在控制台中显示具体的mysql操作
+    // operatorsAliases:false,
+    define: {
+        chartset: "utf8",
+        dialectOptions: {
+            collate: "utf8_general_ci"
+        }
+    },
     dialectOptions:{
         //字符集
         charset:'utf8mb4',
@@ -17,14 +24,18 @@ const mySequelize = new Sequelize('gcx','root','12345678',{
         acquire: 30000,
         idle: 10000
     },
-    timezone: '+08:00'  //东八时区  一定要这么设置才能按照北京时间来记录时间相关的数据
+    timezone: '+08:00', //东八时区  一定要这么设置才能按照北京时间来记录时间相关的数据
+    timestamps: false,
+
 });
 
 // force: true 当我们添加字段的时候数据库会自动删除原先的表格并更新字段,开发阶段可以设置为true
 // 注意在生产环境下千万不要将其设置为true，它会把所有的记录都删除掉
 // 所以我们还是设置为false比较安全，添加字段，我们还是手动删除原先的表格然后刷新加载比较好
-mySequelize.sync({
-    force: true
+sequelize.sync({
+    force: false
 })
 
-module.exports = mySequelize
+module.exports = {
+    sequelize
+}
